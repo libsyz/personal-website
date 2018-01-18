bc437678f570e78ba33bfd7275eb83725affc9b5
miguel jimenez <denkatalabeno@gmail.com>
2018-01-18 15:39:17 +0100

    scrolling implemented;

--- source/javascripts/smoothscroll.js
+++ source/javascripts/smoothscroll.js
@@ -1,12 +1,17 @@
-$('a[href^="#"]').on('click', function(event) {
+
+$( document ).ready(function() {
+  $('a[href^="#"]').on('click', function(event) {
 
     var target = $(this.getAttribute('href'));
 
     if( target.length ) {
+        console.log(target)
         event.preventDefault();
         $('html, body').stop().animate({
             scrollTop: target.offset().top
-        }, 1000);
+        }, 800);
     }
 
 });
+});
+
